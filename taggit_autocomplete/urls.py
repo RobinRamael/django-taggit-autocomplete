3e7416e7bf9af95a38ef97400d3eea16b6763f90
Robin Ramael <robin.ramael@gmail.com>
2014-06-24 15:11:57 +0200

    url and patterns functions are in django.conf.urls in newer versions of django

--- taggit_autocomplete/urls.py
+++ taggit_autocomplete/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import url, patterns
 
 urlpatterns = patterns('taggit_autocomplete.views',
     url(r'^list$', 'list_tags', name='taggit_autocomplete-list'),
