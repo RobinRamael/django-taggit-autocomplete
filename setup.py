72f800b840ddee0e1bd6c62db776526f0ca90f66
Robin Ramael <robin.ramael@gmail.com>
2014-06-24 15:19:11 +0200

    changed author and bumped version

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
- 
+
 long_description = open('README.txt').read()
- 
+
 setup(
     name='django-taggit-autocomplete',
-    version='0.1',
+    version='0.2',
     description='Autocompletion for django-taggit',
     long_description=long_description,
-    author='Jeremy Epstein',
-    author_email='jazepstein@gmail.com',
-    url='http://github.com/Jaza/django-taggit-autocomplete/',
+    author='Robin Ramael',
+    author_email='robin.ramael@gmail.com',
+    url='http://github.com/RobinRamael/django-taggit-autocomplete/',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -22,4 +22,4 @@ setup(
         'Framework :: Django',
     ],
     zip_safe=False,
-) 
+)
